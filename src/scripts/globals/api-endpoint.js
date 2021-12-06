84576935b3df0416a58a2eb1d8a61f3a5309bde9
GedeDana <73019594+GedeDana@users.noreply.github.com>
2021-12-06 11:27:21 +0800

    menampilkan api base category

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,6 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   LIST: `${CONFIG.BASE_URL}api/games`,
   NEW_RELEASE: `${CONFIG.BASE_URL}api/games?sort-by=release-date`,
+  FILTER_BY_CATEGORY: `${CONFIG.BASE_URL}/api/games?category=`,
 };
 
 export default API_ENDPOINT;
--- src/scripts/views/templates/templeate-creator.js
+++ src/scripts/views/templates/templeate-creator.js
@@ -24,4 +24,21 @@ const createGameNewRelase = (game) => `
   </div>
 </div>
 `;
-export { createGamesRecomendation, createGameNewRelase };
+
+const createGameByCategory = (game) => `
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${game.thumbnail}" class="img-fluid rounded-start" alt="${game.title} pitcure">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${game.title}</h5>
+        <p class="card-text">${game.short_description}</p>
+        <p class="card-text"><small class="text-muted">${game.genre}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+export { createGamesRecomendation, createGameNewRelase, createGameByCategory };
