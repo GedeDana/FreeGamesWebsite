606ee07197f30651419d288d9ed8155d9e007f96
GedeDana <73019594+GedeDana@users.noreply.github.com>
2021-12-09 21:15:11 +0800

    menambah api endpoint untuk game list

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -4,6 +4,7 @@ const API_ENDPOINT = {
   LIST: `${CONFIG.BASE_URL}api/games`,
   NEW_RELEASE: `${CONFIG.BASE_URL}api/games?sort-by=release-date`,
   FILTER_BY_CATEGORY: `${CONFIG.BASE_URL}/api/games?category=`,
+  GAME_LIST: `${CONFIG.BASE_URL}api/games?sort-by=alphabetical`,
 };
 
 export default API_ENDPOINT;
