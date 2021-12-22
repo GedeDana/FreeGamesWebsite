06ecc95543b89ac3e8a6d144c5a5419c8a1c7544
GedeDana <73019594+GedeDana@users.noreply.github.com>
2021-12-22 13:41:55 +0800

    modifkasi service worker dan add loading

--- src/scripts/views/pages/gameBasePlatformWeb.js
+++ src/scripts/views/pages/gameBasePlatformWeb.js
@@ -7,6 +7,8 @@ const GamesbyPlatformWeb = {
     <div class="card-recommendations" tabindex="0">
       <div class="container">
       <h3 class="title fw-bold text-center">Game List Web Platform</h3>
+        <div id="contaider-loading">
+        </div>
         <div class="row gy-4 row-cols-3" id="game-container">
 
         </div>
@@ -32,8 +34,12 @@ const GamesbyPlatformWeb = {
     `;
   },
   async afterRender() {
-    const gamesByPlatformPc = await TheGamesDbSource.gameBaseBroswerPlatform();
+    const loadingContainer = document.querySelector('#contaider-loading');
     const gamesByPlatformPcContainer = document.querySelector('#game-container');
+    const lodingPages = '<div class="lds-dual-ring"></div>'; // Source html https://loading.io/css/ //
+    loadingContainer.innerHTML = lodingPages;
+    const gamesByPlatformPc = await TheGamesDbSource.gameBaseBroswerPlatform();
+    loadingContainer.innerHTML = '';
     gamesByPlatformPc.forEach((game) => {
       gamesByPlatformPcContainer.innerHTML += createGamesRecomendation(game);
     });
--- src/scripts/views/pages/gameBasePlatformpc.js
+++ src/scripts/views/pages/gameBasePlatformpc.js
@@ -7,6 +7,8 @@ const GamesbyPlatformPc = {
     <div class="card-recommendations" tabindex="0">
       <div class="container">
       <h3 class="title fw-bold text-center">Game List Windows Platform</h3>
+        <div id="contaider-loading">
+        </div>
         <div class="row gy-4 row-cols-3" id="game-container">
 
         </div>
@@ -32,8 +34,13 @@ const GamesbyPlatformPc = {
     `;
   },
   async afterRender() {
-    const gamesByPlatformPc = await TheGamesDbSource.gameBasePcPlatform();
+    const loadingContainer = document.querySelector('#contaider-loading');
     const gamesByPlatformPcContainer = document.querySelector('#game-container');
+    const lodingPages = '<div class="lds-dual-ring"></div>'; // Source html https://loading.io/css/ //
+    loadingContainer.innerHTML = lodingPages;
+    const gamesByPlatformPc = await TheGamesDbSource.gameBasePcPlatform();
+    loadingContainer.innerHTML = '';
+
     gamesByPlatformPc.forEach((game) => {
       gamesByPlatformPcContainer.innerHTML += createGamesRecomendation(game);
     });
